e7577c58ba2776ddda33dbd79d60cc112b56da4f
Dikshith99 <dikshith4222@gmail.com>
2026-02-16 22:54:37 +0530

    Initial commit UC1

---
+++ Oopbanner.java
@@ -0,0 +1,14 @@
+public class Oopbanner {
+    public static void main(String[]args){
+        System.out.println("Oop");
+    }
+    public static void main1(String[] args){
+        System.out.println("   ***   " + "   ***   " + "");
+        System.out.println(" **   ** " + " **   ** " + "");
+        System.out.println("**     **" + "**     **" + "");
+        System.out.println("**     **" + "**     **" + "");
+        System.out.println("**     **" + "**     **" + "");
+        System.out.println(" **   ** " + " **   ** " + "");
+        System.out.println("   ***   " + "   ***   " + "");
+    }
+}
